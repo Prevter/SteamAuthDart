7ce165180f8ed5e8f3865c1763626148abe3c7a8
Oleksandr Nemesh <prevter@gmail.com>
2022-05-26 00:09:33 +0300

    Update util.dart

--- lib/src/util.dart
+++ lib/src/util.dart
@@ -1,23 +1,17 @@
 import 'dart:typed_data';
 
 class Util {
+  /// Returns timestamp in milliseconds
   static int getSystemUnixTime() {
     return DateTime.now().millisecondsSinceEpoch ~/ 1000;
   }
 
-  static Uint8List hexStringToBytes(String hex) {
-    int hexLen = hex.length;
-    var ret = List.filled(hexLen ~/ 2, 0);
-    for (int i = 0; i < hexLen; i += 2) {
-      ret[i ~/ 2] = int.parse(hex.substring(i, i + 2), radix: 16);
-    }
-    return Uint8List.fromList(ret);
-  }
-
+  /// Converts hex string to BigInt
   static BigInt hexStringToBigInt(String hex) {
     return BigInt.parse(hex, radix: 16);
   }
 
+  /// Converts cookie map to a header string
   static String stringifyCookies(Map<String, String> cookies) =>
       cookies.entries.map((e) => '${e.key}=${e.value}').join('; ');
 }
