fb1fbd5a3764fe5bef0285acebd8597a919d24c9
Oleksandr Nemesh <prevter@gmail.com>
2022-05-26 00:41:53 +0300

    Update steam_auth_test.dart

--- test/steam_auth_test.dart
+++ test/steam_auth_test.dart
@@ -3,4 +3,8 @@ import 'package:steam_auth/steam_auth.dart';
 void main() async {
   // Important to call this first
   await TimeAligner.alignTimeAsync();
+
+  test('aligned', () {
+    expect(TimeAligner.aligned, true);
+  });
 }
